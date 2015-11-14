7c9dc23963e783c5664d2f47d229362e66742645
Ken Tran <loschorts@gmail.com>
2015-11-13 17:54:04 -0800

    finish

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,6 +3,8 @@ require_relative 'db_connection'
 require_relative '01_sql_object'
 
 module Searchable
+
+
   def where(params)
     where_line =
       params.keys
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,4 +1,6 @@
 require_relative '03_associatable'
+require 'byebug'
+
 
 # Phase IV
 module Associatable
@@ -6,6 +8,19 @@ module Associatable
 
   def has_one_through(name, through_name, source_name)
     through_options = assoc_options[through_name]
-  
+
+    define_method name do
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through = through_options.model_class
+      through_id = through_options.foreign_key
+      source = source_options.model_class
+
+      call = source_options.foreign_key
+
+      source_id = through.find(self.send(through_id)).send(call)
+      source_options.model_class.find(source_id)
     end
+
+  end
 end
