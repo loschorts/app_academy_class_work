e9e75b6d7ceb0a75c7229d9366fa8a315952cd11
App Academy Student <student@appacademy.io>
2015-11-18 14:05:29 -0800

    add find_by_credentials and password setter for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,22 @@ class User < ActiveRecord::Base
     SecureRandom::urlsafe_base64(16)
   end
 
+  def password=
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
   def reset_session_token!
     self.session_token = self.class.generate_session_token
     self.save!
     self.session_token
+  end
+
+  def self.find_by_credentials(user_name, password)
+    User.find_by(username: user_name, password: password)
   end
 
   private
