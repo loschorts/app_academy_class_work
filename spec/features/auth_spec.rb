41da0f126e4297b1913a11e463a4bd45f50f35b3
App Academy Student <student@appacademy.io>
2015-11-23 13:34:59 -0800

    add sign in feature

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -1,50 +1,60 @@
 require 'spec_helper'
 require 'rails_helper'
+describe GoalsApp do
+  let(:test_user) { FactoryGirl.create(:user) }
 
-
-feature "the signup process" do
-let(:test_user) { FactoryGirl.create(:user) }
-
-  before :each do
-    visit "/users/new"
-  end
-
-  it "has a new user page" do
-    expect(page).to have_content("Sign Up")
-  end
-
-  feature "signing up a user" do
-
-    it "shows username on the homepage after signup" do
-      fill_in "Username", with: test_user.username
-      fill_in "Password", with: test_user.password
-      click_button "Sign Up"
-      expect(page).to have_content(test_user.username)
-    end
-  end
-
-end
-
-feature "logging in" do
-let(:test_user) { FactoryGirl.create(:user) }
-
-  before :each do
-    visit new_session_url
+  def fill_in_user_info
     fill_in "Username", with: test_user.username
     fill_in "Password", with: test_user.password
-    click_button "Sign In"
   end
 
-  it "shows username on the homepage after login" do
-    expect(page).to have_content(test_user.username)
+  feature "the signup process" do
+  let(:test_user) { FactoryGirl.create(:user) }
+
+    before :each do
+      visit "/users/new"
+      fill_in_user_info
+    end
+
+    it "has a new user page" do
+      expect(page).to have_content("Sign Up")
+    end
+
+    feature "signing up a user" do
+
+      it "shows username on the homepage after signup" do
+        fill_in "Username", with: test_user.username
+        fill_in "Password", with: test_user.password
+        click_button "Sign Up"
+        expect(page).to have_content(test_user.username)
+      end
+    end
+
   end
 
-end
+  feature "logging in" do
+  let(:test_user) { FactoryGirl.create(:user) }
 
-feature "logging out" do
+    before :each do
+      visit new_session_url
+      fill_in_user_info
+      click_button "Sign In"
+    end
 
-  it "begins with logged out state"
+    it "shows username on the homepage after login" do
+      expect(page).to have_content(test_user.username)
+    end
 
-  it "doesn't show username on the homepage after logout"
+  end
+
+  feature "logging out" do
+
+    it "begins with logged out state" do
+
+    end
+
+    it "doesn't show username on the homepage after logout"
+
+  end
 
 end
