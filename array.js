36b40430716375e908e423fb17e631d552b79fc7
App Academy Student <student@appacademy.io>
2015-11-26 13:44:20 -0800

    write array.uniq

---
+++ array.js
@@ -0,0 +1,22 @@
+Array.prototype.uniq = function () {
+  var uniques = [];
+  this.forEach(function(element){
+      var match = false;
+    uniques.forEach(function(uniqElement){
+      if (uniqElement === element){
+        match = true;
+      }
+    });
+
+    if (match === false){ uniques.push(element) }
+
+  });
+    // if (uniques.find(element) === undefined)
+    // uniques.push(element);
+  return uniques;
+};
+var array = [1,2,3,4,5,5];
+var uniqs = array.uniq();
+
+console.log(array);
+console.log(uniqs);
